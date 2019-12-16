12a46232bea27612b60c5934e56785c68ece0aac
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2019-12-16 15:55:37 +0100

    Ssortof works, description!!!
    a difference vektorbol ki kell szedni  a %2 -t.

--- Assets/Scripts/GenerateBoard.cs
+++ Assets/Scripts/GenerateBoard.cs
@@ -8,6 +8,7 @@ public class GenerateBoard : MonoBehaviour
     float scale;
     List<float> origoX = new List<float>();
     List<float> origoY = new List<float>();
+    int a;
 
     void Start()
     {
@@ -20,12 +21,13 @@ public class GenerateBoard : MonoBehaviour
         origoY.Add(y);
         Instantiate(tile,new Vector3(x, y, 0), Quaternion.Euler(0, 0, 60 * (origoX.Count - 1)));
         Vector2 difference = new Vector2(-scale * Mathf.Sqrt(3) / 4, 0.25f * scale);
-        difference = Quaternion.Euler(0, 0, 60 * origoX.Count) * difference;
+        difference = Quaternion.Euler(0, 0, 60 * ((origoX.Count - 1) % 2)) * difference;
         float newX = origoX[origoX.Count - 1] + difference.x;
         float newY = origoY[origoY.Count - 1] + difference.y;
         //if (!ContainsList(newX, newY))
-        if(Mathf.Abs(x) < 60)
+        if(a < 5)
         {
+            a++;
             Origo(newX, newY);
         }
     }

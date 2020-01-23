06cb93ae30a1e7ff2f24d6984cadb1107498d3af
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2020-01-23 12:40:03 +0100

    Cleaned up Laoder

--- Assets/Scripts/BoardLoader.cs
+++ Assets/Scripts/BoardLoader.cs
@@ -20,43 +20,51 @@ public class BoardLoader : MonoBehaviour
         tileAmount = ReadInt();
         wallAmount = ReadInt();
 
+        LoadTiles(tileAmount);
+        LoadWalls(wallAmount);
+    }
+
+    void LoadTiles (int tileAmount)
+    {
         for (int i = 0; i < tileAmount; i++)
         {
-            float x;
-            float y;
-            float rotZ;
-            int neighbourAmount;
-
-            x = ReadFloat();
-            y = ReadFloat();
-            rotZ = ReadFloat();
-            neighbourAmount = ReadInt();
-
-            TileData tmp = Instantiate(tile, new Vector3(x, y, 0), Quaternion.Euler(0, 0, rotZ)).GetComponent<TileData>();
-
-            for (int j = 0; j < neighbourAmount; j++)
-            {
-                tmp.neighbours.Add(ReadInt());
-            }
+            LoadTile();
         }
+    }
 
+    void LoadWalls(int wallAmount)
+    {
         for (int i = 0; i < wallAmount; i++)
         {
-            float x;
-            float y;
-            float rotZ;
-            int neighbour1;
-            int neighbour2;
-
-            x = ReadFloat();
-            y = ReadFloat();
-            rotZ = ReadFloat();
-
-            Wall tmp = Instantiate(wall, new Vector3(x, y, 0), Quaternion.Euler(0, 0, rotZ)).GetComponent<Wall>();
-
-            tmp.neighbour1 = ReadInt();
-            tmp.neighbour2 = ReadInt();
+            LoadWall();
         }
+    }
+
+    void LoadTile()
+    {
+        float x = ReadFloat();
+        float y = ReadFloat();
+        float rotZ = ReadFloat();
+        int neighbourAmount = ReadInt();
+
+        TileData tmp = Instantiate(tile, new Vector3(x, y, 0), Quaternion.Euler(0, 0, rotZ)).GetComponent<TileData>();
+
+        for (int j = 0; j < neighbourAmount; j++)
+        {
+            tmp.neighbours.Add(ReadInt());
+        }
+    }
+
+    void LoadWall()
+    {
+        float x = ReadFloat();
+        float y = ReadFloat();
+        float rotZ = ReadFloat();
+
+        Wall tmp = Instantiate(wall, new Vector3(x, y, 0), Quaternion.Euler(0, 0, rotZ)).GetComponent<Wall>();
+
+        tmp.neighbour1 = ReadInt();
+        tmp.neighbour2 = ReadInt();
     }
 
     int ReadInt ()

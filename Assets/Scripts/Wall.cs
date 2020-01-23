5d17aecb0771e9c6cd0bff18ce503e1526d33005
mtotbagi <55552289+mtotbagi@users.noreply.github.com>
2020-01-23 14:42:43 +0100

    Walls fixed rotation

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -16,10 +16,12 @@ public class Wall : MonoBehaviour
         Vector3 pos2 = FindObjectOfType<GenerateBoard>().tiles[neighbour2].transform.position;
 
         Vector3 targetPos = (pos1 + pos2) / 2;
+        Vector3 vector1 = (pos2 - pos1);
+        Vector2 vector2 = new Vector3(vector1.y, -vector1.x);
+        float angle = Vector2.SignedAngle(new Vector2(1, 0),vector2);
+		Quaternion targetRot = Quaternion.Euler(0f, 0f, angle);
 
-		Quaternion targetRot = Quaternion.Euler(0f, 0f, extraRot + FindObjectOfType<GenerateBoard>().tiles[neighbour2].transform.rotation.eulerAngles.z);
-
-		transform.position = targetPos;
+        transform.position = targetPos;
 
 		transform.rotation = targetRot;
 

002a93a2f25d7d230aa53b510b7ad6a86099cae2
mtotbagi <55552289+mtotbagi@users.noreply.github.com>
2020-01-23 12:40:16 +0100

    BoardSave added

---
+++ Assets/Scripts/BoardSaver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class BoardSaver : MonoBehaviour
+{
+    List<GameObject> tiles = FindObjectOfType<GenerateBoard>().tiles;
+    List<GameObject> walls = FindObjectOfType<GenerateBoard>().walls;
+    List<string> output = new List<string>();
+    string outputFileName;
+
+    public void Save()
+    {
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            WriteTile(i);
+        }
+        for (int i = 0; i < walls.Count; i++)
+        {
+            WriteWall(i);
+        }
+        File.WriteAllLines(outputFileName, output);
+    }
+
+    public void WriteTile(int i)
+    {
+        output.Add(tiles[i].transform.position.x.ToString());
+        output.Add(tiles[i].transform.position.y.ToString());
+        output.Add(tiles[i].transform.rotation.z.ToString());
+        output.Add(tiles[i].GetComponent<Tile>().data.neighbours.Count.ToString());
+        for (int j = 0; j < tiles[i].GetComponent<Tile>().data.neighbours.Count; j++)
+        {
+            output.Add(tiles[i].GetComponent<Tile>().data.neighbours[j].ToString());
+        }
+
+    }
+    public void WriteWall(int i)
+    {
+        output.Add(walls[i].transform.position.x.ToString());
+        output.Add(walls[i].transform.position.y.ToString());
+        output.Add(walls[i].transform.rotation.z.ToString());
+        output.Add(walls[i].GetComponent<Wall>().neighbour1.ToString());
+        output.Add(walls[i].GetComponent<Wall>().neighbour2.ToString());
+    }
+}

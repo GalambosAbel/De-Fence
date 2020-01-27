a7ef0611eec9be47a6020cefe7f9b5d4058c50fe
mtotbagi <mtotbagi@users.noreply.github.com>
2020-01-27 12:53:35 +0100

    Added AbstractBoard, not done yet

---
+++ Assets/Scripts/Abstract/AbstractBoard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractBoard
+{
+    public List<AbstractTile> tiles;
+    public List<AbstractWall> walls;
+    public List<List<int>> graph;
+
+    public AbstractBoard()
+    {
+        tiles = new List<AbstractTile>();
+        walls = new List<AbstractWall>();
+        graph = new List<List<int>>();
+    }
+
+    //tiles must be ordered by the tile's ID in ascending order
+    public AbstractBoard(List<AbstractTile> _tiles, List<AbstractWall> _walls)
+    {
+        tiles = _tiles;
+        walls = _walls;
+        graph = new List<List<int>>();
+
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            List<int> tmp = new List<int>();
+            for (int j = 0; j < tiles[i].neighbours.Count; j++)
+            {
+                tmp.Add(tiles[i].neighbours[j][0]);
+            }
+            graph.Add(tmp);
+        }
+    }
+}

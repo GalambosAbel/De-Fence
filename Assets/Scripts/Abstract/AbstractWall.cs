42cf40df77f58d22f959ff32bd0ec40d553a1a02
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2020-01-27 15:29:40 +0100

    removed neighbours of a.wall

--- Assets/Scripts/Abstract/AbstractWall.cs
+++ Assets/Scripts/Abstract/AbstractWall.cs
@@ -6,12 +6,10 @@ public class AbstractWall
 {
     public int ID;
     public bool active;
-    public int[] neighbours;
 
-    public AbstractWall(int _ID, int neighbour1, int neighbour2)
+    public AbstractWall(int _ID)
     {
         ID = _ID;
         active = true;
-        neighbours = new int[] { neighbour1, neighbour2 };
     }
 }

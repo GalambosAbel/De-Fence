2bef618e33eb99f3a56d9f225af4d5f90ce8722a
mtotbagi <mtotbagi@users.noreply.github.com>
2020-02-06 19:06:07 +0100

    Random color

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,7 +9,17 @@ public class Tile : MonoBehaviour
 
     void Update()
     {
-		
+        gameObject.GetComponent<SpriteRenderer>().color = GetColor();
+    }
+
+    Color GetColor()
+    {
+        int x = AbstractManager.board.tiles[ID].owner;
+        if (x == 0) return Color.white;
+        if (x == 1) return Color.red;
+        if (x == 2) return Color.blue;
+        if (x == 3) return Color.green;
+        return new Color(((243 - x) *(123 + x) - 3 * x + 22) % 256, (x * x * x + 12 * x - 17) % 256, (x * 2 * x + 11 * x + 5) % 256);
     }
 }
 //distanceOffset = 2f  angleOffset = 60f

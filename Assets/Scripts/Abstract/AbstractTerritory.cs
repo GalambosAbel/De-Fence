2629b6be8ba35d6cca29852a8c9e0449a4ac4c6e
mtotbagi <mtotbagi@users.noreply.github.com>
2020-02-23 17:42:47 +0100

    territories update correctly

--- Assets/Scripts/Abstract/AbstractTerritory.cs
+++ Assets/Scripts/Abstract/AbstractTerritory.cs
@@ -31,4 +31,33 @@ public class AbstractTerritory
 			return _strength;
 		}
     }
+
+
+    public void CleanUp()
+    {
+        if(Strength == 0)
+        {
+            foreach(int tile in tiles)
+            {
+                AbstractManager.board.tiles[tile].owner = 0;
+                foreach (int[] neighbour in AbstractManager.board.tiles[tile].neighbours)
+                {
+                    AbstractManager.board.walls[neighbour[1]].active = true;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 1; i < tiles.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (AbstractManager.board.tiles[tiles[i]].IsNeighbourOf(tiles[j]))
+                    {
+                        AbstractManager.board.tiles[tiles[i]].GetWallOfNeighbour(tiles[j]).active = false;
+                    }
+                }
+            }
+        }
+    }
 }

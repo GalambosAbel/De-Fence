c44c4c8a100a4c1138da49ca18281c8500e26367
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2020-03-04 14:33:50 +0100

    now shows score, minor ui changes

---
+++ Assets/Scripts/ScoreDisplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplayer : MonoBehaviour
+{
+    public Text[] playerScores;
+
+    void Start()
+    {
+        for (int i = 0; i < AbstractManager.playerAmount; i++)
+        {
+            playerScores[i].color = AbstractManager.playerColors[i + 1];
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < AbstractManager.playerAmount; i++)
+        {
+            playerScores[i].text = AbstractManager.GetPlayerScore(i + 1).ToString();
+        }
+    }
+}

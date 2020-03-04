fccb000f0262269b26a2611257b8c5f865dddc76
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2020-03-04 15:17:12 +0100

    camera positions

---
+++ Assets/Scripts/CameraPositioner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioner : MonoBehaviour
+{
+    void Awake()
+    {
+        Camera.main.orthographicSize = 2.8f;
+    }
+}

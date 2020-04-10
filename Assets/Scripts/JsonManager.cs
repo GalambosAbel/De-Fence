d188784e2ab7ab5f33073fb1f2b3d31116306471
mtotbagi <mtotbagi@users.noreply.github.com>
2020-04-10 15:56:59 +0200

    Build

--- Assets/Scripts/JsonManager.cs
+++ Assets/Scripts/JsonManager.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
 using DeFenceAbstract;
-using UnityEngine.WSA;
 
 public class JsonManager : MonoBehaviour
 {
